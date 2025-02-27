39a117650682809a9abbf9754f54248144af2da0
Seven Hsu <xushengwen@echo.tech>
2025-02-28 01:59:42 +0800

    unify the style and the logic

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
+  base: '/yourdays/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
