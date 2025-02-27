0f7722acd75460d00cee8e48085bc6026573265e
Seven Hsu <xushengwen@echo.tech>
2025-02-28 02:23:54 +0800

    unify the style and the logic

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,28 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-const routes = [
-    {
-        path: '/',
-        redirect: '/mydays'
-    },
-    {
-        path: '/mydays',
-        name: 'MyDays',
-        component: () => import('../views/MyDays.vue')
-    },
-    {
-        path: '/yours',
-        name: 'YoursPlan',
-        component: () => import('../views/YoursPlan.vue')
-    },
-    {
-        path: '/addmy',
-        name: 'AddMy',
-        component: () => import('../views/AddMy.vue')
-    }
-]
-
 const router = createRouter({
-    history: createWebHashHistory('/yourdays/'),
-    routes
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: '/',
+            redirect: '/mydays'
+        },
+        {
+            path: '/mydays',
+            name: 'MyDays',
+            component: () => import('../views/MyDays.vue')
+        },
+        {
+            path: '/yours',
+            name: 'YoursPlan',
+            component: () => import('../views/YoursPlan.vue')
+        },
+        {
+            path: '/addmy',
+            name: 'AddMy',
+            component: () => import('../views/AddMy.vue')
+        }
+    ]
 })
 
 export default router
